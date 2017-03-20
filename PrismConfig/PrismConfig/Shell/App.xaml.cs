e57cf7a3ad098935456a0b7b85b80edba235e701
frankwwu <frankwwu@hotmail.com>
2017-03-20 18:47:25 -0400

    PrismConfig

---
+++ PrismConfig/PrismConfig/Shell/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+
+namespace Shell
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+#if (DEBUG)
+            RunInDebugMode();
+#else
+            RunInReleaseMode();
+#endif
+            this.ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+
+        private static void RunInDebugMode()
+        {
+            Bootstrapper bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
+
+        private static void RunInReleaseMode()
+        {
+            AppDomain.CurrentDomain.UnhandledException += AppDomainUnhandledException;
+            try
+            {
+                Bootstrapper bootstrapper = new Bootstrapper();
+                bootstrapper.Run();
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private static void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            HandleException(e.ExceptionObject as Exception);
+        }
+
+        private static void HandleException(Exception ex)
+        {
+            if (ex == null) return;
+
+            MessageBox.Show(Shell.Properties.Resources.UnhandledException);
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ PrismConfig/PrismConfig/Shell/ViewModels/ShellViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Shell.ViewModels
+{
+    public class ShellViewModel : INotifyPropertyChanged
+    {
+        public ShellViewModel()
+        {
+        }
+
+        private UserControl _selectedMenu;
+
+        public UserControl SelectedMenu
+        {
+            get { return _selectedMenu; }
+            set
+            {
+                _selectedMenu = value;
+                MessageBox.Show(_selectedMenu.ToString().Split('.')[0], _selectedMenu.ToString());
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
