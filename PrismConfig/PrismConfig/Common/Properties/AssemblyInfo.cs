c3fd8577abe68f0feff50ef4e133aa30b1dc0740
frankwwu <frankwwu@hotmail.com>
2017-03-22 21:20:30 -0400

    PrismConfig

---
+++ PrismConfig/PrismConfig/Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Common")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c6ca71e-fa4e-4094-a84e-a005f63ddfec")]
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PrismConfig/PrismConfig/Common/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Common
+{
+    /// <summary>
+    /// Defines the names of the shell's UI regions.
+    /// </summary>
+    public static class RegionNames
+    {
+        public const string LeftRegion = "LeftRegion";
+        public const string RightRegion = "RightRegion";
+        public const string StartupMenuRegion = "StartupMenuRegion";
+    }
+}
