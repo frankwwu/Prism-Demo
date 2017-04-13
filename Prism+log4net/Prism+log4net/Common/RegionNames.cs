466f9c7744f7f6235ad7a21a93069c671a4af453
frankwwu <frankwwu@hotmail.com>
2017-04-13 19:16:33 -0400

    Prism+log4net

---
+++ Prism+log4net/Prism+log4net/Common/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Common
+{
+    /// <summary>
+    /// Defines the names of the shell's UI regions.
+    /// </summary>
+    public static class RegionNames
+    {
+        public const string TopLeftRegion = "TopLeftRegion";
+        public const string BottomLeftRegion = "BottomLeftRegion";
+        public const string MainRegion = "MainRegion";
+    }
+}
